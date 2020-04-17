ed287373fc00339e0a738985bc39b3f64a8674b4
Joshua Akangah <akangah89@hmail.com>
2020-04-17 01:50:51 +0000

    new java assignment

---
+++ MusicOrganizer.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+
+/**
+ * A class to hold details of audio files.
+ * 
+ * @author Joshua Akangah
+ * @version 17/04/20
+ */
+public class MusicOrganizer
+{
+    // An ArrayList for storing the file names of music files.
+    private ArrayList<String> files;
+        
+    /**
+     * Create a MusicOrganizer
+     */
+    public MusicOrganizer()
+    {
+        files = new ArrayList<String>();
+    }
+    
+    /**
+     * Add a file to the collection.
+     * @param filename The file to be added.
+     */
+    public void addFile(String filename)
+    {
+        files.add(filename);
+    }
+    
+    /**
+     * Return the number of files in the collection.
+     * @return The number of files in the collection.
+     */
+    public int getNumberOfFiles()
+    {
+        return files.size();
+    }
+    
+    /**
+     * List a file from the collection.
+     * @param index The index of the file to be listed.
+     */
+    public void listFile(int index)
+    {
+        if(index >= 0 && index < files.size()) {
+            String filename = files.get(index-1);
+            if (index == 0){
+                System.out.println("Index does not exist");
+            }else{
+                System.out.println(index + ": " + filename);
+            }
+            
+        }else{
+            System.out.println("Index does not exist");
+        }
+    }
+    
+    /**
+     * Remove a file from the collection.
+     * @param index The index of the file to be removed.
+     */
+    public void removeFile(int index)
+    {
+        if(index >= 0 && index < files.size()) {
+            files.remove(index);
+        }else{
+            System.out.println("Index does not exist");
+        }
+    }
+}
